d6cd529f8c5e3da18b1e51f6157ebadb3f0db8a1
Konatsup <konatsukomatsu@gmail.com>
2018-09-06 13:21:32 +0900

    add 削除機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,18 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def create
     @task = Task.new(title: params[:title])
     if @task.save
       flash[:success] = "\"" + @task.title+ "\"" + "を追加しました"
+      redirect_to('/')
+    end
+  end
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    if @task.destroy
+      flash[:success] = "\"" + @task.title+ "\"" + "を削除しました"
       redirect_to('/')
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'tasks#index'
   post 'tasks/create' => 'tasks#create'
+  delete 'tasks/:id/destroy' => 'tasks#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
