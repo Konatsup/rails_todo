5ac916111921ebb0f3323de3aff0e3c7c8261461
Konatsup <konatsukomatsu@gmail.com>
2018-09-06 13:09:59 +0900

    add create task action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,13 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  def create
+    @task = Task.new(title: params[:title])
+    if @task.save
+      flash[:success] = "\"" + @task.title+ "\"" + "を追加しました"
+      redirect_to('/')
+    else
+    end
+  end
+
 end
