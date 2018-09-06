e1d11646bb1ea7689547bc404168a839e5cb63f7
Konatsup <konatsukomatsu@gmail.com>
2018-09-06 13:09:34 +0900

    add flash style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  get '/' => 'tasks#index'
+  post 'tasks/create' => 'tasks#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
